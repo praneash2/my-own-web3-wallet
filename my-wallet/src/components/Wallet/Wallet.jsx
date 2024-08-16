313f48ebda6f08ba5ae7f9575840d132f298a877
praneash2 <praneash2@gmail.com>
2024-08-17 00:26:26 +0530

    fixed the bug in balance

--- my-wallet/src/components/Wallet/Wallet.jsx
+++ my-wallet/src/components/Wallet/Wallet.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react'
 
 export default function Wallet({value,index}) {
   const [balance,setBalance] =useState(0);
-  useEffect(()=>{
-    async()=>{
+  useEffect( ()=>{
+    
+      async function fetchData(){
         let payload={
           "jsonrpc": "2.0",
           "id": 1,
@@ -13,19 +14,28 @@ export default function Wallet({value,index}) {
           value[0]
           ]
         }
-        const data=await fetch("https://solana-mainnet.g.alchemy.com/v2/81CXWBI9avABQ-yl46bQVf1PvzRh8FU_", {
+        const response=await fetch("https://solana-mainnet.g.alchemy.com/v2/81CXWBI9avABQ-yl46bQVf1PvzRh8FU_", {
           method: 'POST', // Specify the request method
           headers: {
             'Content-Type': 'application/json' // Set the content type to JSON
           },
           body: JSON.stringify(payload) // Convert the payload to a JSON string
         })
-        console.log("balance",data); 
-    }
+        let {result}=await response.json();
+        console.log(result?.value);
+        setBalance(result?.value);
+      }
+     
+      fetchData()
+   
     
-  },[]);
+  });
+  
   return (
     <div>
+      <p>
+        balance:{balance}
+      </p>
       <p>wallet {index}</p>
       Public Key : {value[0]}
       <br>
